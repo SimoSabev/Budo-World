11954849710887ffeb97090edaad9dfe1fba395c
IvayloKraev <ivaylokraev@protonmail.com>
2024-03-19 18:52:23 +0200

    Move sign out to Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,44 @@
 // Navbar.js
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 import styles from "./Navbar.module.css";
+import {onAuthStateChanged, signOut} from "firebase/auth";
+import {auth} from "../../firebase-config";
 
-const Navbar = () => {
-  return (
-    <nav className={styles.nav_bar}>
-      <div className={styles.left}>
-        <li className={styles.li_1}>
-          <Link to="/">Начало</Link>
-        </li>
-      </div>
-      <div className={styles.middle}>
-        <li className={styles.li_2}>
-          <Link to="/martial-arts">Бойни изкуства</Link>
-        </li>
-      </div>
-      <div className={styles.right}>
-        <li className={styles.li_3}>
-          <Link to="/auth/login">Вписване</Link>
-        </li>
-        <li className={styles.li_3}>
-          <Link to="/auth/signup">Регистрация</Link>
-        </li>
-      </div>
-    </nav>
-  );
+export default function Navbar() {
+    const [user, setUser] = useState<typeof auth.currentUser>(null); // TODO: Make it a type dependent
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) =>
+            setUser(currentUser || null)
+        );
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <nav className={styles.nav_bar}>
+            <div className={styles.left}>
+                <li className={styles.li_1}>
+                    <Link to="/">Начало</Link>
+                </li>
+            </div>
+            <div className={styles.middle}>
+                <li className={styles.li_2}>
+                    <Link to="/martial-arts">Бойни изкуства</Link>
+                </li>
+            </div>
+            <div className={styles.right}>
+                <li className={styles.li_3}>
+                    {(user && user.uid) ? (
+                            <a type={"button"} onClick={() => signOut(auth)}>
+                                Отписване
+                            </a>
+                        ) :
+                        <Link to="/auth/login">Вписване</Link>
+                    }
+                </li>
+            </div>
+        </nav>
+    );
 };
-
-export default Navbar;
