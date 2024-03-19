8af87255bc189af1218fef43c0b4ffe787e6d287
IvayloKraev <ivaylokraev@protonmail.com>
2024-03-19 19:02:48 +0200

    Update authentication logic

--- src/pages/auth/Login/Login.tsx
+++ src/pages/auth/Login/Login.tsx
@@ -1,93 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { auth } from '../../../firebase-config';
+import React, {useState, useEffect, FormEvent} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {onAuthStateChanged, signInWithEmailAndPassword} from 'firebase/auth';
+import {auth} from '../../../firebase-config';
 import styles from './Login.module.css';
 
-const Login = () => {
-  const [loginEmail, setLoginEmail] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
-  const [user, setUser] = useState<any>(null); // TODO: Make it a type dependent
-  const [loggedIn, setLoggedIn] = useState(false);
+export default function Login() {
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+    const [user, setUser] = useState<typeof auth.currentUser>(null); // TODO: Make it a type dependent
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser || null);
-    });
+    const navigate = useNavigate();
 
-    return () => unsubscribe();
-  }, []);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => setUser(currentUser));
 
-  const login = async () => {
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-      const user = userCredential.user;
-      console.log(user);
-      setLoggedIn(true); // Set loggedIn state to true after successful login
-    } catch (error: any) {     // TODO: Make it a type dependent
-      console.error(error.message);
-    }
-  };
+        return () => unsubscribe();
+    }, []);
 
-  const signout = async () => {
-    try {
-      await signOut(auth);
-      setLoggedIn(false); // Set loggedIn state to false after signout
-    } catch (error: any) {      // TODO: Make it a type dependent
-      console.error(error.message);
-    }
-  };
+    useEffect(() => {
+        if(user && user.uid) navigate("/martial-arts")
+    }, [user]);
 
-  return (
-    <div className={styles.app}>
-      <div className={styles.shadow_box}>
-        <div className={styles.Content}>
-          <h3 className={styles.Header}> Впиши се </h3>
-          <input
-            className={styles.Email}
-            placeholder="Имейл..."
-            onChange={(event) => {
-              setLoginEmail(event.target.value);
-            }}
-          />
-          <input
-            className={styles.Password}
-            placeholder="Парола..."
-            type="password"
-            onChange={(event) => {
-              setLoginPassword(event.target.value);
-            }}
-          />
-          {loggedIn ? (
-            <Link to="/">
-              <button className={styles.button}>
-                Продължете напред
-              </button>
-            </Link>
-          ) : (
-            <button className={styles.button} onClick={login}>
-              Влезте в профила си
-            </button>
-          )}
+    const login = async (submit: FormEvent) => {
+        submit.preventDefault();
+        await signInWithEmailAndPassword(auth, loginEmail, loginPassword).catch((reason) => reason);
+    };
+
+    return (
+        <div className={styles.app}>
+            <div className={styles.shadow_box}>
+                <form onSubmit={login} className={styles.Content}>
+                    <h3 className={styles.Header}> Впиши се </h3>
+                    <input
+                        className={styles.Email}
+                        placeholder="Имейл..."
+                        onChange={(event) => {
+                            setLoginEmail(event.target.value);
+                        }}
+                    />
+                    <input
+                        className={styles.Password}
+                        placeholder="Парола..."
+                        type="password"
+                        onChange={(event) => {
+                            setLoginPassword(event.target.value);
+                        }}
+                    />
+                    <button className={styles.button} type={"submit"}>
+                        Влезте в профила си
+                    </button>
+                </form>
+
+                <div className={styles.link_element}>
+                    <p className={styles.link1}>Нямате акаунт? </p>
+                    <Link className={styles.link2} to="/auth/signup">
+                        Регистрирай се
+                    </Link>
+                </div>
+            </div>
         </div>
-
-        <div className={styles.link_element}>
-          <p className={styles.link1}>Нямате акаунт? </p>
-          <Link className={styles.link2} to="/auth/signup">
-            Регистрирай се
-          </Link>
-        </div>
-      </div>
-      {!loggedIn && (
-        <div className={styles.user}>
-          {user?.email} &nbsp;&nbsp;&nbsp;
-          <button className={styles.signout} onClick={signout}>
-            Отписване
-          </button>
-        </div>
-      )}
-    </div>
-  );
+    );
 };
-
-export default Login;
--- src/pages/auth/Signup/Signup.tsx
+++ src/pages/auth/Signup/Signup.tsx
@@ -1,84 +1,64 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
-import { auth } from "../../../firebase-config";
+import React, {useState, useEffect, FormEvent} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {createUserWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth';
+import {auth} from "../../../firebase-config";
 import styles from './Signup.module.css';
 
 export default function Signup() {
-   const [registerEmail, setRegisterEmail] = useState('');
-   const [registerPassword, setRegisterPassword] = useState('');
-   const [user, setUser] = useState<any>(null);
-   const [registered, setRegistered] = useState(false);
+    const [registerEmail, setRegisterEmail] = useState('');
+    const [registerPassword, setRegisterPassword] = useState('');
+    const [user, setUser] = useState<any>(null);
 
-   useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-         setUser(currentUser || null); // Set to null if currentUser is null
-      });
+    const navigate = useNavigate();
 
-      return () => unsubscribe(); // Cleanup function to unsubscribe when component unmounts
-   }, []);
 
-   const signup = async () => {
-      try {
-         const userCredential = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
-         const user = userCredential.user;
-         console.log(user);
-         setRegistered(true); // Set registered state to true after successful signup
-      } catch (error: any) { // TODO: Make it a type dependent
-         console.error(error.message);
-      }
-   };
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser || null); // Set to null if currentUser is null
+        });
 
-   const signout = async () => {
-      try {
-         await signOut(auth);
-      } catch (error: any) { // TODO: Make it a type dependent
-         console.error(error.message);
-      }
-   };
+        return () => unsubscribe(); // Cleanup function to unsubscribe when component unmounts
+    }, []);
 
-   return (
-      <div className={styles.app}>
-         <div className={styles.shadow_box}>
-            <div className={styles.Content}>
-               <h3 className={styles.Header}> Регистрирай се </h3>
-               <input
-                  className={styles.Email}
-                  placeholder="Имейл..."
-                  onChange={(event) => {
-                     setRegisterEmail(event.target.value);
-                  }}
-               />
-               <input
-                  className={styles.Password}
-                  placeholder="Парола..."
-                  type="password"
-                  onChange={(event) => {
-                     setRegisterPassword(event.target.value);
-                  }}
-               />
-               {!registered && <button className={styles.button} onClick={signup}> Създай профил</button>}
-               {registered && (
-                  <Link to="/martial-arts">
-                     <button className={styles.button}>
-                        Напред
-                     </button>
-                  </Link>
-               )}
+    useEffect(() => {
+        if (user && user.uid) navigate("/martial-arts")
+    }, [user]);
+
+    const signup = async (submit: FormEvent) => {
+        submit.preventDefault();
+        await createUserWithEmailAndPassword(auth, registerEmail, registerPassword).catch((reason) => reason);
+    };
+
+    return (
+        <div className={styles.app}>
+            <div className={styles.shadow_box}>
+                <form onSubmit={signup} className={styles.Content}>
+                    <h3 className={styles.Header}> Регистрирай се </h3>
+                    <input
+                        className={styles.Email}
+                        placeholder="Имейл..."
+                        onChange={(event) => {
+                            setRegisterEmail(event.target.value);
+                        }}
+                    />
+                    <input
+                        className={styles.Password}
+                        placeholder="Парола..."
+                        type="password"
+                        onChange={(event) => {
+                            setRegisterPassword(event.target.value);
+                        }}
+                    />
+                    <button className={styles.button} type={"submit"}>
+                        Създай профил
+                    </button>
+                </form>
+
+                <div className={styles.link_element}>
+                    <p className={styles.link1}>Вече имате акаунт? </p>
+                    <Link className={styles.link2} to="/auth/login">Впиши се</Link>
+                </div>
             </div>
-
-            <div className={styles.link_element}>
-               <p className={styles.link1}>Вече имате акаунт? </p>
-               <Link className={styles.link2} to="/auth/login">Впиши се</Link>
-            </div>
-         </div>
-
-         <div className={styles.user}>
-            {user?.email} &nbsp;&nbsp;&nbsp;
-            {!registered && (
-               <button className={styles.signout} onClick={signout}> Отписване </button>
-            )}
-         </div>
-      </div>
-   );
+        </div>
+    );
 }
