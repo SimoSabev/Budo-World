f95fb5aaf8f7a838b740e1b0434911c8e46c8de8
IvayloKraev <ivaylokraev@protonmail.com>
2024-03-21 13:43:50 +0200

    Create interface and collections for liked martial arts

---
+++ src/Models/ILikedMartialArts.ts
@@ -0,0 +1,3 @@
+export interface ILikedMartialArts {
+    liked: string[];
+}
--- src/firestore/collections.ts
+++ src/firestore/collections.ts
@@ -4,6 +4,7 @@ import {IDetailedMartialArt} from "../Models/martial-arts/IDetailedMartialArt";
 import {CollectionReference} from "@firebase/firestore";
 import {IMartialArtCard} from "../Models/MartialArtCard";
 import {ISportsClub} from "../Models/SportsClub";
+import {ILikedMartialArts} from "../Models/ILikedMartialArts";
 
 export function createCollection<T>(collectionName: string) {
     return collection(db, collectionName) as CollectionReference<T>
@@ -14,3 +15,5 @@ export const MartialArtsCardsCollection = createCollection<IMartialArtCard>("mar
 export const MartialArtsDetailedCollection = createCollection<IDetailedMartialArt>("martial-arts-detailed");
 
 export const SportsClubsCollection = createCollection<ISportsClub>("sports-club");
+
+export const LikedMartialArtsCollection = createCollection<ILikedMartialArts>("liked-martial-arts")
