5dc5a22f3e3c0a2996673beec375aac29d28cbf2
IvayloKraev <ivaylokraev@protonmail.com>
2024-03-22 23:33:07 +0200

    Updated question type

--- src/Models/Question.ts
+++ src/Models/Question.ts
@@ -3,7 +3,6 @@ export interface IQuestion {
     question: string;
     options: {
         option: string,
-        points: number,
-        // resultType: string[]
+        resultType: string[]
     }[];
 }
